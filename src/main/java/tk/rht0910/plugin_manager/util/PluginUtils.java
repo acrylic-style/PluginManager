f815eed852456e107699689e97763889dc8df4ca
rht0910 <rht0910@outlook.jp>
2017-06-21 23:04:00 +0900

    already initialized! error fix

--- src/main/java/tk/rht0910/plugin_manager/util/PluginUtils.java
+++ src/main/java/tk/rht0910/plugin_manager/util/PluginUtils.java
@@ -171,8 +171,7 @@ public final class PluginUtils {
 			sender.sendMessage(ChatColor.RED + "An error occurred: " + e);
 			return false;
 		} finally {
-			sender.sendMessage(ChatColor.GREEN
-					 + "Unloaded plugin: " + name);
+			sender.sendMessage(ChatColor.GREEN + "Unloaded plugin: " + name);
 		}
 		}
 		return true;
@@ -372,7 +371,6 @@ public final class PluginUtils {
 		} finally {
 			try {
 				fw.close();
-				sender.sendMessage("Writed file: " + file.toString());
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
