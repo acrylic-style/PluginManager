de72e5b34f13266228aba734fb8bc1e05c1de1b0
rht0910 <rht0910@outlook.jp>
2018-03-11 01:17:20 +0900

    Wrong arguments fixed...

--- src/main/java/tk/rht0910/plugin_manager/PluginManager.java
+++ src/main/java/tk/rht0910/plugin_manager/PluginManager.java
@@ -281,9 +281,9 @@ public final class PluginManager extends JavaPlugin implements TabCompleter, Lis
 					sender.sendMessage(ChatColor.translateAlternateColorCodes(altColorChar, Lang.not_enough_args));
 					return false;
 				} else if(args.length == 2) {
-					PluginUtils.loadPlugin(sender, args[0]);
+					PluginUtils.loadPlugin(sender, args[1]);
 				} else if(args.length > 3) {
-					PluginUtils.loadPlugin(sender, args[0]);
+					PluginUtils.loadPlugin(sender, args[1]);
 					sender.sendMessage(ChatColor.YELLOW + "Note: Two arguments are not supported. Use /pman load <Plugin>");
 				}
 			} else if(args[0].equalsIgnoreCase("disable")) {
