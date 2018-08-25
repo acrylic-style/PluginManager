b6e5614201725fe1ce154af6ddcf04521798d770
rht0910 <rht0910@outlook.jp>
2018-08-25 09:25:35 +0900

    some problems are resolved

--- src/main/java/tk/rht0910/plugin_manager/thread/VersionCheck.java
+++ src/main/java/tk/rht0910/plugin_manager/thread/VersionCheck.java
@@ -95,7 +95,7 @@ public class VersionCheck extends Thread implements Runnable {
 					sender.sendMessage(ChatColor.translateAlternateColorCodes(altColorChar, Lang.version_check_complete_update1));
 					sender.sendMessage(ChatColor.translateAlternateColorCodes(altColorChar, String.format(Lang.version_check_complete_update2, PluginManager.current)));
 					sender.sendMessage(ChatColor.translateAlternateColorCodes(altColorChar, String.format(Lang.version_check_complete_update3, line + ChatColor.RED + edition)));
-					if(edition.equals("(dev)")) {
+					if("(dev)".equals(edition)) {
 						sender.sendMessage(ChatColor.translateAlternateColorCodes(altColorChar, Lang.version_check_complete_update5));
 					} else {
 						sender.sendMessage(ChatColor.translateAlternateColorCodes(altColorChar, Lang.version_check_complete_update4));
